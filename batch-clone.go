3b3bf8bd81db0921cb094b85cc1af5b9a1235f8b
Adam Hepner <adam@hepner.eu>
2019-08-22 23:35:59 +0200

    implement

---
+++ batch-clone.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/urfave/cli.v2"
+)
+
+// RepoEntry is the format used by GH to report all basic information about a repo
+// I only need one position here, so I'm sticking to basics
+type RepoEntry struct {
+	GitURL string `json:"git_url"`
+}
+
+func main() {
+	app := cli.App{}
+
+	var username string
+	var token string
+
+	app.Flags = append(app.Flags, &cli.StringFlag{
+		Name:        "username",
+		Destination: &username,
+	}, &cli.StringFlag{
+		Name:        "access-token",
+		Destination: &token,
+	})
+
+	app.Action = func(c *cli.Context) error {
+		req := http.Request{
+			Method: "GET",
+			URL: &url.URL{
+				Scheme: "https",
+				Host:   "api.github.com",
+				Path:   "/user/repos",
+			},
+			Header: make(http.Header, 0),
+		}
+
+		headers := req.Header
+		headers.Add("accept", "application/vnd.github.v3+json")
+		if len(token) > 0 {
+			headers.Add("authorization", "token "+token)
+		}
+
+		client := http.Client{}
+
+		resp, err := client.Do(&req)
+		if err != nil {
+			return err
+		}
+
+		log.WithFields(log.Fields{"Status": resp.Status}).Info("Got response")
+
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(resp.Body)
+
+		data := []RepoEntry{}
+
+		err = json.Unmarshal(buf.Bytes(), &data)
+		if err != nil {
+			log.WithFields(log.Fields{"err": err}).Fatal("Cannot unmarshal GH's respnse")
+		}
+
+		for _, e := range data {
+			fmt.Println(e.GitURL)
+		}
+
+		return nil
+	}
+
+	app.Run(os.Args)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/AdamHepner/batch-clone
+
+go 1.12
+
+require (
+	github.com/sirupsen/logrus v1.4.2
+	gopkg.in/urfave/cli.v2 v2.0.0-20190806201727-b62605953717
+)
